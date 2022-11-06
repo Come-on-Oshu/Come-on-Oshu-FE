f5ddf639733cec0b30ecd15a45803512b57ea302
Yujin_nKim <yujin.kimd@gmail.com>
2022-11-06 22:06:24 +0900

    feature: implement login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,57 @@
-import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-void main() => runApp(const MyApp());
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late GoogleMapController mapController;
-
-  final LatLng _center = const LatLng(45.521563, -122.677433);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
-      ),
-    );
-  }
-}
-// import 'package:flutter/cupertino.dart';
 // import 'package:flutter/material.dart';
-// import 'package:oshucome/screen/DetailInformationScreen.dart';
-// import 'package:oshucome/screen/EventListScreen.dart';
-// import 'package:oshucome/screen/LoginScreen.dart';
-// import 'package:oshucome/screen/MyPageScreen.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
 //
-// void main() => runApp(MyApp());
+// void main() => runApp(const MyApp());
 //
-// class MyApp extends StatelessWidget {
+// class MyApp extends StatefulWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//   late GoogleMapController mapController;
+//
+//   final LatLng _center = const LatLng(45.521563, -122.677433);
+//
+//   void _onMapCreated(GoogleMapController controller) {
+//     mapController = controller;
+//   }
+//
 //   @override
 //   Widget build(BuildContext context) {
 //     return MaterialApp(
-//       home: LoginScreen()
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Maps Sample App'),
+//           backgroundColor: Colors.green[700],
+//         ),
+//         body: GoogleMap(
+//           onMapCreated: _onMapCreated,
+//           initialCameraPosition: CameraPosition(
+//             target: _center,
+//             zoom: 11.0,
+//           ),
+//         ),
+//       ),
 //     );
 //   }
 // }
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:oshucome/screen/DetailInformationScreen.dart';
+import 'package:oshucome/screen/EventListScreen.dart';
+import 'package:oshucome/screen/LoginScreen.dart';
+import 'package:oshucome/screen/MyPageScreen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginScreen()
+    );
+  }
+}
--- lib/screen/LoginScreen.dart
+++ lib/screen/LoginScreen.dart
@@ -1,23 +1,87 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
+
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final useridcontroller = TextEditingController();
+  final userpwcontroller = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
-
-    return SafeArea(//SafeArea위젯은 기기의 상단 노티바 부분, 하단 영역을 침범하지 않는 안전한 영역 잡아주는 위젯
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('oshu'),
-          backgroundColor: Colors.deepPurpleAccent,
-        ),
-        body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FeCo In Daejeon'),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding( //main image
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: Container(
+                    width: 300,
+                    height: 200,
+                    child: Image.asset('images/festival.jpg')),
+              ),
+            ),
+            Padding( //user id field
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: SizedBox(
+                width: 300,
+                child: TextField(
+                  controller: useridcontroller,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'ID',
+                    hintText: 'Enter valid ID',),
+                ),
+              )
+            ),
+            Padding(//user pw field
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 15, bottom: 15),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: SizedBox(
+                width: 300,
+                child: TextField(
+                  controller: userpwcontroller,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      hintText: 'Enter password'),
+                ),
+              )
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 50,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.deepPurpleAccent, borderRadius: BorderRadius.circular(10)),
+              child: TextButton(
+                onPressed: () {
+                  print(useridcontroller.text);
+                  print(userpwcontroller.text);
+                },
+                child: Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 130,
+            ),
+          ],
         ),
       ),
     );
